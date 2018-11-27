9b2cc3bf81bfc7ab8eaf7886f56694df4bd1ceca
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-11-26 18:05:49 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ sudo mv google_assistant /usr/bin/
 #Icon
 sudo mv google-assistant.png /usr/share/icons/
 
+#Sound effect
+sudo mv open-ended.mp3 /usr/share/assistant/
+
 #Desktop file
 sudo mv GoogleAssistant.desktop /usr/share/applications/
