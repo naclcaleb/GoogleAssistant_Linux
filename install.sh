074cf03c86ba25eb489b664a9d0691d5b40cfb90
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-11-26 18:00:53 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+#Google Assistant Executable
+chmod +x google_assistant
+sudo mv google_assistant /usr/bin/
+
+#Icon
+sudo mv google-assistant.png /usr/share/icons/
+
+#Desktop file
+sudo mv GoogleAssistant.desktop /usr/share/applications/
